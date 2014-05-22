d7629c2591053ca3cf5207421245ba72cd76d4c0
alex1313 <sav.1209@gmail.com>
2014-05-23 00:02:38 +0600

    Priority, MultiValue and NegativeNumbers tests passed, implement NegativeNumbers test

--- Calculator/CalculatorTest/UnitTest1.cs
+++ Calculator/CalculatorTest/UnitTest1.cs
@@ -17,7 +17,25 @@ namespace CalculatorTest
         [TestMethod]
         public void PrimitiveExpression()
         {
-            Assert.AreEqual(4, calc.Calculate("2 + 2"));
+            Assert.AreEqual(1, calc.Calculate("2 / 2"));
+        }
+
+        [TestMethod]
+        public void PriorityExpression()
+        {
+            Assert.AreEqual(2, calc.Calculate("(2+ 2* 4 )/ 5"));
+        }
+
+        [TestMethod]
+        public void MultiValueDigitExpression()
+        {
+            Assert.AreEqual(92, calc.Calculate("2+ 2* 45 + 20/ 5 - 4"));
+        }
+
+        [TestMethod]
+        public void NegativeNumbersExpression()
+        {
+            Assert.AreEqual(-4, calc.Calculate("-2 * (3 + -1)"));
         }
     }
 }
