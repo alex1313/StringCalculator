1918e0ebfeeac5383bc12edf7e19eb0b34d495c3
alex1313 <sav.1209@gmail.com>
2014-05-22 20:40:21 +0600

    Initialization, test#1

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Program
+    {
+        public static int Calculate(string expression)
+        {
+            return 0;
+        }
+
+        static void Main(string[] args)
+        {
+            string expression = Console.ReadLine();
+            Console.WriteLine("{0} = {1}", expression, Calculate(expression));
+        }
+    }
+}
---
+++ Calculator/CalculatorTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator;
+
+namespace CalculatorTest
+{
+    [TestClass]
+    public class ExpressionsTest
+    {
+        [TestMethod]
+        public void RegularExpression()
+        {
+            Assert.AreEqual(4, Program.Calculate("2 + 2"));
+        }
+    }
+}
